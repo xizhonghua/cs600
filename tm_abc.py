1e09bbbcab0f9f299613a2a600871133b2a57e73
Zhonghua Xi <xizhonghua@gmail.com>
2015-09-16 22:23:14 -0400

    update

--- tm_abc.py
+++ tm_abc.py
@@ -11,114 +11,53 @@ class TM_abc():
 
   def __init__(self):
 
+    self.delta = {
+        'q_0': {
+            ' ': {'next': 'q_acc'},
+            'x': {'next': 'q_0', 'move': 'R'},
+            'a': {'next': 'q_1', 'move': 'R', 'val': 'x'},
+            '*': {'next': 'q_rej'}
+        },
+        'q_1': {
+            'ax': {'next': 'q_1', 'move': 'R'},
+            'b': {'next': 'q_2', 'move': 'R', 'val': 'x'},
+            '*': {'next': 'q_rej'}
+        },
+        'q_2': {
+            'bx': {'next': 'q_2', 'move': 'R'},
+            'c': {'next': 'q_3', 'move': 'R', 'val': 'x'},
+            '*': {'next': 'q_rej'}
+        },
+        'q_3': {
+            ' ': {'next': 'q_5', 'move': 'L'},
+            'c': {'next': 'q_4', 'move': 'R'},
+            '*': {'next': 'q_rej'}
+        },
+        'q_4': {
+            'c': {'next': 'q_4', 'move': 'R'},
+            ' ': {'next': 'q_5', 'move': 'L'},
+            '*': {'next': 'q_rej'}
+        },
+        'q_5': {
+            ' ': {'next': 'q_0', 'move': 'R'},
+            '*': {'next': 'q_5', 'move': 'L'},
+        }
+    }
     return
-
-  """
-  Get the char in head position
-  """
-
-  def get(self):
-    return self.x[self.head]
 
   def move(self, d, val=None):
     if val is not None:
       self.x[self.head] = val
-
-    # markers = [' ' for i in range(0, len(self.x))]
-    # markers[self.head] = '^'
-
-    # print ''.join(self.x), self.state
-    # print ''.join(markers)
 
     if d == 'L':
       self.head -= 1
     if d == 'R':
       self.head += 1
 
-  def q_0(self):
-    self.state = 0
-    c = self.get()
-    if c == ' ':
-      return self.q_acc()
-    elif c == 'x':
-      self.move('R')
-      return self.q_0()
-    elif c == 'a':
-      self.move('R', 'x')
-      return self.q_1()
-    else:
-      return self.q_rej()
-
-  def q_1(self):
-    self.state = 1
-    c = self.get()
-    if c == 'a' or c == 'x':
-      self.move('R')
-      return self.q_1()
-    elif c == 'b':
-      self.move('R', 'x')
-      return self.q_2()
-    else:
-      return self.q_rej()
-
-  def q_2(self):
-    self.state = 2
-    c = self.get()
-    if c == 'b' or c == 'x':
-      self.move('R')
-      return self.q_2()
-    elif c == 'c':
-      self.move('R', 'x')
-      return self.q_3()
-    else:
-      return self.q_rej()
-
-  def q_3(self):
-    self.state = 3
-    c = self.get()
-    if c == ' ':
-      self.move('L')
-      return self.q_5()
-    elif c == 'c':
-      self.move('R')
-      return self.q_4()
-    else:
-      self.move('R')
-      return self.q_rej()
-
-  def q_4(self):
-    self.state = 4
-    c = self.get()
-    if c == 'c':
-      self.move('R')
-      return self.q_4()
-    elif c == ' ':
-      self.move('L')
-      return self.q_5()
-    else:
-      self.move('R')
-      return self.q_rej()
-
-  def q_5(self):
-    self.state = 5
-    c = self.get()
-    if c == ' ':
-      self.move('R')
-      return self.q_0()
-    else:
-      self.move('L')
-      return self.q_5()
-
-  def q_acc(self):
-    self.state = 'acc'
-    return 'accept'
-
-  def q_rej(self):
-    self.state = 'rej'
-    return 'reject'
-
   def run(self, x):
     """
+    >>> TM_abc().run('')
+    'accept'
     >>> TM_abc().run('abc')
     'accept'
     >>> TM_abc().run('aaabbbccc')
@@ -134,7 +73,29 @@ class TM_abc():
     """
     self.x = list(' ' + x + ' ')
     self.head = 1
-    return self.q_0()
+    self.state = 'q_0'
+    while True:
+      if self.state == 'q_acc':
+        return 'accept'
+      if self.state == 'q_rej':
+        return 'reject'
+
+      c = self.x[self.head]
+
+      key = '*'
+      for chars in self.delta[self.state]:
+        if c in chars:
+          key = chars
+          break
+
+      delta = self.delta[self.state][key]
+
+      d = delta['move'] if 'move' in delta else None
+      val = delta['val'] if 'val' in delta else None
+      self.move(d, val)
+      self.state = delta['next']
+
+    return 'reject'
 
 
 if __name__ == '__main__':
